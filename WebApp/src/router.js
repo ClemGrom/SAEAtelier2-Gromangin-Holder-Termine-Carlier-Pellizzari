f5355c6c7059f832d02f2e655263d201d3113d33
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2024-02-05 11:44:31 +0100

    feat/front : maj for imports

--- WebApp/src/router.js
+++ WebApp/src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import page404 from '../components/page404.vue'
-import Acceuil from '../components/Acceuil.vue'
+import page404 from './views/page404.vue'
+import Acceuil from './views/Acceuil.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
