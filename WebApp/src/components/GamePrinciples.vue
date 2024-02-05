386cf97d57f9b1553bbfbc820a4c8ad0f345b795
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2024-02-05 14:10:43 +0100

    feat/front : login,register,home views

---
+++ WebApp/src/components/GamePrinciples.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h2 class="section-title">Principe et règles du jeu</h2>
+
+    <p class="game-description">
+      GeoQuizz est un jeu captivant où votre tâche est de placer avec précision des photos sur une carte de votre ville, plus rapidement que les autres joueurs. Voici comment le jeu fonctionne :
+    </p>
+
+    <ul class="game-rules">
+      <li>Une partie consiste en une séquence de 10 photos aléatoires d'une même série, à placer sur la carte de la ville.</li>
+      <li>Chaque série regroupe des photos de la même ville et de la même carte.</li>
+      <li>Vous gagnez des points en fonction de la précision du placement et de la rapidité de votre réponse.</li>
+      <li>L'objectif est d'obtenir le maximum de points au cours d'une partie de 10 photos.</li>
+      <li>La partie se termine une fois que toutes les 10 photos ont été positionnées.</li>
+    </ul>
+
+    <h3 class="scoring-title">Règles de calcul des points :</h3>
+    <ul class="scoring-rules">
+      <li>Pour une réponse placée à une distance inférieure à D : 5 points.</li>
+      <li>Pour une réponse placée à une distance inférieure à 2D : 3 points.</li>
+      <li>Pour une réponse placée à une distance inférieure à 3D : 1 point.</li>
+      <li>Les points sont multipliés par 4 pour une réponse en moins de 5 secondes.</li>
+      <li>Les points sont multipliés par 2 pour une réponse en moins de 10 secondes.</li>
+      <li>Aucun point n'est acquis pour une réponse en plus de 20 secondes.</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // TODO ajouter des propriétés ou des méthodes spécifiques au composant ici
+};
+</script>
+
+<style scoped>
+.game-description {
+  font-size: 16px;
+  color: #333;
+  margin-bottom: 15px;
+}
+
+.section-title {
+  font-size: 24px;
+  color: black;
+  margin-bottom: 10px;
+  font-weight: bold;
+  text-decoration: underline;
+
+}
+
+.game-rules {
+  font-size: 14px;
+  color: #555;
+  list-style-type: disc;
+  margin-left: 20px;
+}
+
+.scoring-title {
+  font-size: 20px;
+  color: #e74c3c;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  text-decoration: underline;
+}
+
+.scoring-rules {
+  font-size: 14px;
+  color: #e74c3c;
+  list-style-type: disc;
+  margin-left: 20px;
+}
+</style>
---
+++ WebApp/src/views/Acceuil.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="home-container">
+    <div class="header">
+      <h1 class="main-title">GeoQuizz</h1>
+      <div class="user-actions">
+        <router-link to="/register" class="cta-button">pas encore inscrit ?</router-link>
+        <router-link to="/login" class="cta-button">Se connecter</router-link>
+      </div>
+    </div>
+    <div class="logo-actions">
+      <img src="@/assets/game_logo.jpg" alt="Game Logo" class="game-logo" />
+      <div class="extra-actions">
+        <router-link to="/profile" class="cta-button">consulter mon profil</router-link>
+        <router-link to="/create-game" class="cta-button">Créer une partie !</router-link>
+      </div>
+    </div>
+    <div class="game-intro">
+      <p class="game-intro-text">
+        Bienvenue dans GeoQuizz, le jeu captivant où vous devez placer avec précision des photos sur la carte de votre ville plus rapidement que les autres joueurs. Prêt pour l'aventure ?
+      </p>
+    </div>
+    <div class="game-principles">
+      <GamePrinciples />
+    </div>
+  </div>
+</template>
+
+<script>
+import GamePrinciples from '@/components/GamePrinciples.vue';
+
+export default {
+  components: {
+    GamePrinciples,
+  },
+};
+</script>
+
+<style scoped>
+.home-container {
+  text-align: center;
+  background-color: #2c3e50;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  color: #ecf0f1;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.main-title {
+  font-size: 48px;
+  color: #e74c3c;
+  margin-bottom: 20px;
+  font-weight: bold;
+}
+
+.user-actions {
+  display: flex;
+  gap: 10px;
+}
+
+.logo-actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.extra-actions {
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+}
+
+.cta-button {
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 16px;
+  color: #ecf0f1;
+  background-color: #3498db;
+  border: none;
+  border-radius: 5px;
+  text-decoration: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.cta-button:hover {
+  background-color: #2980b9;
+}
+
+.game-intro {
+  margin-bottom: 30px;
+}
+
+.game-logo {
+  width: 150px;
+  height: 150px;
+  margin-bottom: 30px;
+}
+
+.game-intro-text {
+  font-size: 18px;
+  color: #ecf0f1;
+}
+
+.game-principles {
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
+
