07551e8c1b9ff67ca57242ae7e0b2bfd0355099b
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2024-02-06 16:02:46 +0100

    feat/front : maj final v1 front

--- WebApp/src/components/GamePrinciples.vue
+++ WebApp/src/components/GamePrinciples.vue
@@ -1,92 +1,135 @@
 <template>
-  <div>
-    <div class="game">
-          <h2 class="section-title">Principe et règles du jeu</h2>
-  
-    <p class="game-description">
-      GeoQuizz est un jeu captivant où votre tâche est de placer avec précision des photos sur une carte de votre ville, plus rapidement que les autres joueurs. Voici comment le jeu fonctionne :
-    </p>
-
-    <ul class="game-rules">
-      <li>Une partie consiste en une séquence de 10 photos aléatoires d'une même série, à placer sur la carte de la ville.</li>
-      <li>Chaque série regroupe des photos de la même ville et de la même carte.</li>
-      <li>Vous gagnez des points en fonction de la précision du placement et de la rapidité de votre réponse.</li>
-      <li>L'objectif est d'obtenir le maximum de points au cours d'une partie de 10 photos.</li>
-      <li>La partie se termine une fois que toutes les 10 photos ont été positionnées.</li>
-    </ul>
-  </div>
-
-  <div class="scoring">
-    <h3 class="scoring-title">Règles de calcul des points :</h3>
-    <ul class="scoring-rules">
-      <li>Pour une réponse placée à une distance inférieure à D : 5 points.</li>
-      <li>Pour une réponse placée à une distance inférieure à 2D : 3 points.</li>
-      <li>Pour une réponse placée à une distance inférieure à 3D : 1 point.</li>
-      <li>Les points sont multipliés par 4 pour une réponse en moins de 5 secondes.</li>
-      <li>Les points sont multipliés par 2 pour une réponse en moins de 10 secondes.</li>
-      <li>Aucun point n'est acquis pour une réponse en plus de 20 secondes.</li>
-    </ul>
-  </div>
+  <div class="home-container">
+    <div class="gray-container">
+      <div class="content-container">
+        <div class="content-section" :class="{ 'active': showPrinciples }" @click="togglePrinciplesDisplay">
+          <h2 class="section-title" @mouseover="hoverEffect" @mouseleave="leaveEffect">Principe et règles du jeu</h2>
+          <div class="divider"></div>
+        </div>
+        <div class="content-section" :class="{ 'active': showPoints }" @click="togglePointsDisplay">
+          <h2 class="section-title" @mouseover="hoverEffect" @mouseleave="leaveEffect">Calculer ses points </h2>
+          <div class="divider"></div>
+        </div>
+      </div>
+      <div class="text-container">
+        <div v-show="showPrinciples" class="text-content">
+          <ul class="game-rules">
+            <li>Une partie consiste en une séquence de 10 photos aléatoires d'une même série, à placer sur la carte de la ville.</li>
+            <li>Chaque série regroupe des photos de la même ville et de la même carte.</li>
+            <li>Vous gagnez des points en fonction de la précision du placement et de la rapidité de votre réponse.</li>
+            <li>L'objectif est d'obtenir le maximum de points au cours d'une partie de 10 photos.</li>
+            <li>La partie se termine une fois que toutes les 10 photos ont été positionnées.</li>
+          </ul>
+        </div>
+        <div v-show="showPoints" class="text-content">
+          <ul class="game-rules">
+            <p>Placement des réponses</p>
+            <li>pour 1 réponse placée à une distance &lt; D : <span class="point-value">5pts</span></li>
+            <li>pour 1 réponse placée à une distance &lt; 2D : <span class="point-value">3pts</span></li>
+            <li>pour 1 réponse placée à une distance &lt; 3D : <span class="point-value">1pts</span></li>
+          </ul>
+          <ul class="game-rules">
+            <p>Prise en compte de la rapidité</p>
+            <li>les points sont multipliés par 4 pour une réponse en moins de 5s</li>
+            <li>les points sont multipliés par 2 pour 1 réponse en moins de 10s</li>
+            <li>les points ne sont pas acquis pour 1 réponse en plus de 20s</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // TODO ajouter des propriétés ou des méthodes spécifiques au composant ici
+  data() {
+    return {
+      showPrinciples: true,
+      showPoints: false
+    };
+  },
+  methods: {
+    togglePrinciplesDisplay() {
+      this.showPrinciples = !this.showPrinciples;
+      this.showPoints = false;
+    },
+    togglePointsDisplay() {
+      this.showPoints = !this.showPoints;
+      this.showPrinciples = false;
+    },
+    hoverEffect(event) {
+      event.target.style.cursor = 'pointer';
+    },
+    leaveEffect(event) {
+      event.target.style.cursor = 'default';
+    }
+  }
 };
 </script>
 
 <style scoped>
-div{
-  display: grid;
-  grid-template-columns: 12 1fr 1fr;
-  grid-gap: 20px;
-  color: black;
-  
-}
-.game{
-  grid-column: 1/5;
-  background-color: white;
-  padding: 20px;
-  border-radius: 20px;
-}
-.scoring{
-  grid-column: 5/13;
-  background-color: white;
-  padding: 20px;
-  border-radius: 20px;
-}
-.game-description {
-  font-size: 16px;
- 
+body {
+  font-family: Arial, sans-serif;
+  background-color: #22364B;
+  padding-bottom: 100px;
 }
 
-.section-title {
-  font-size: 24px;
-  color: black;
+.home-container {
+  text-align: center;
+  padding: 20px;
+  color: #ecf0f1;
+}
 
+.gray-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.content-container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.content-section {
+  flex: 1;
+  text-align: center;
   font-weight: bold;
-  text-decoration: underline;
-
+  color : white;
 }
+
+.active {
+  color: black;
+}
+
+.text-container {
+  margin-top: 20px;
+}
+
+.text-content {
+  padding: 20px;
+  border-radius: 8px;
+  background-color: white;
+  color: #000000;
+  margin-bottom: 50px;
+  width: 600px;
+}
+
 
 .game-rules {
-  font-size: 14px;
- 
-  margin-left: 20px;
-  list-style: none;
+  font-size: 18px;
 }
 
-.scoring-title {
+.game-rules li {
+  margin-bottom: 10px;
+  line-height: 1.6;
+}
+
+.game-rules p {
   font-size: 20px;
-  
- 
-  text-decoration: underline;
+  margin-bottom: 10px;
+  text-align: left;
 }
 
-.scoring-rules {
-  font-size: 14px;
-
-  list-style: none;
-}
 </style>
--- WebApp/src/router.js
+++ WebApp/src/router.js
@@ -7,6 +7,7 @@ import page404 from "./views/page404.vue";
 import jeu from "./views/jeu.vue";
 import FinRound from "./views/FinRound.vue";
 import FinJeu from "./views/FinJeu.vue";
+import Profile from "@/views/Profile.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -39,6 +40,12 @@ const router = createRouter({
         name:"FinJeu",
         component: FinJeu,
      },
+
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+    },
 
     {
       path: "/:pathMatch(.*)*",
