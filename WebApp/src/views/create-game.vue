9a51d5ffbaf370488c4ff79a72e6a5e4ef081282
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-02-07 15:48:33 +0100

    s

--- WebApp/src/views/create-game.vue
+++ WebApp/src/views/create-game.vue
@@ -1,29 +1,36 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center vh-100 bg-dark">
-        <div class="row w-100 text-dark bg-light">
-            <div class="col-lg-12">
-                <h2 class="main-title text-center">Choix de la difficulté : </h2>
-                <div class="d-flex justify-content-around mt-5">
-                    <button class="btn text-success" :class="{ 'bg-button': difficulty === 'easy' }" @click="setDifficulty('easy')">Facile</button>
-                    <button class="btn text-warning" :class="{ 'bg-button': difficulty === 'normal' }" @click="setDifficulty('normal')">Normal</button>
-                    <button class="btn text-danger" :class="{ 'bg-button': difficulty === 'hard' }" @click="setDifficulty('hard')">Difficile</button>
+    <div class="d-flex justify-content-center align-items-center vh-100 bg-custom">
+        <div class="row w-100 text-white">
+            <h2 class="main-title text-center rounded">Choix de la difficulté : </h2>
+            <div class="col-lg-12 ">
+                    <div class="d-flex justify-content-around mt-5 text-dark ">
+                        <button class="btn text-white btn-no-hover" :class="{ 'bg-success': difficulty === 'easy' }"
+                            @click="setDifficulty('easy')">Facile</button>
+                        <button class="btn text-white btn-no-hover" :class="{ 'bg-warning': difficulty === 'normal' }"
+                            @click="setDifficulty('normal')">Normal</button>
+                        <button class="btn text-white btn-no-hover" :class="{ 'bg-danger': difficulty === 'hard' }"
+                            @click="setDifficulty('hard')">Difficile</button>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-12">
-                <h2 class="main-title text-center">Choix de la série</h2>
-                <div class="mt-5 series-container row">
-                    <div v-for="series in seriesOptions" :key="series.value" class="series-card col-lg-3 mb-2">
-                        <h3>{{ series.text }}</h3>
-                        <p>{{ series.description }}</p>
-                        <p>Meilleur score : {{ series.bestScore }}</p>
-                        <button class="btn btn-success" @click="setSeries(series.value)">Choisir cette série</button>
+            <div class="bg-white rounded">
+               
+                <div class="col-lg-12 bg-white text-dark">
+                    <h2 class="main-title text-center">Choix de la série</h2>
+                    <div class="mt-5 series-container row ">
+                        <div v-for="series in seriesOptions" :key="series.value"
+                            class="series-card col-lg-3 mb-2 bg-custom text-white rounded">
+                            <h3>{{ series.text }}</h3>
+                            <p>{{ series.description }}</p>
+                            <p>Meilleur score : {{ series.bestScore }}</p>
+                            <button class="btn btn-success" @click="setSeries(series.value)">Choisir cette série</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
+  
 <script>
 export default {
     data() {
@@ -35,7 +42,7 @@ export default {
                 { value: 'series2', text: 'Série 2', description: 'Description de la série 2', bestScore: 200 },
                 { value: 'series3', text: 'Série 3', description: 'Description de la série 3', bestScore: 300 },
                 { value: 'series4', text: 'Série 4', description: 'Description de la série 4', bestScore: 400 },
-                { value: 'series5', text: 'Série 5', description: 'Description de la série 5', bestScore: 500}
+                { value: 'series5', text: 'Série 5', description: 'Description de la série 5', bestScore: 500 }
             ],
         };
     },
@@ -52,13 +59,13 @@ export default {
     },
 };
 </script>
-
+  
 <style scoped>
-
+.bg-custom {
+    background-color: #22364B;
+}
 
 .row {
-    color: #000;
-    background-color: #f8f9fa;
     margin-left: 5%;
     margin-right: 5%;
 }
@@ -77,7 +84,7 @@ export default {
     border-radius: 5px;
     text-decoration: none;
     cursor: pointer;
-    transition: background-color 0.3s;
+    background-color: #22364B;
 }
 
 .btn-success {
@@ -89,8 +96,12 @@ export default {
     background-color: #2980b9;
 }
 
+.btn-no-hover:hover {
+    background-color: inherit !important;
+}
+
 .bg-button {
-    background-color: #bababa6e;
+    background-color: #534f4f6e;
 }
 
 .series-container {
@@ -102,5 +113,5 @@ export default {
     padding: 20px;
     margin: 20px;
     margin-left: 70px;
-}
-</style>
+}</style>
+  
