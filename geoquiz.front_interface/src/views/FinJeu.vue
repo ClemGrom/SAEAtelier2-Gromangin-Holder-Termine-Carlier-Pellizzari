50a0209d211c2787394916b4f62e61740185fcbf
Maxime48 <maxime48@users.noreply.github.com>
2024-02-09 16:23:36 +0100

    Debug print

--- geoquiz.front_interface/src/views/FinJeu.vue
+++ geoquiz.front_interface/src/views/FinJeu.vue
@@ -66,6 +66,9 @@ export default {
     },
     // Soumission du score au serveur
     submitScore() {
+      console.log('Id du jeu :', this.idgame);
+      console.log('Score total :', this.totalScore);
+      console.log('Token :', this.token);
       this.apiClient.post(`http://docketu.iutnc.univ-lorraine.fr:5015/api/games/${this.idgame}/submit/`, {
         // Données à envoyer dans la requête
 
